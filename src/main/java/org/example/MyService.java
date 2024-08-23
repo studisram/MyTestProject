a192ea819e614a1b89d06253d72a43bab0d848df
stud_i_sram <gurkin.work@gmail.com>
2024-08-23 15:24:21 +0300

    Add MyService.java with testing method

---
+++ src/main/java/org/example/MyService.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class MyService {
+    // Метод, помеченный аннотацией @Loggable, который будет логироваться при вызове
+    @Loggable
+    public void doSomething(String param){
+        // Что-то делает
+    }
+}
