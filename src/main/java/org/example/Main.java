fe0317aa390f583102180d81f19a66de573e5cb1
stud_i_sram <gurkin.work@gmail.com>
2024-08-23 15:38:47 +0300

    Add MyService.java with testing method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,20 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.lang.reflect.*; // Импортируем классы для работы с рефлексией
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws Exception {
+        MyService service = new MyService(); // Создаем экземпляр класса MyService
+        LoggingAspect aspect = new LoggingAspect(); // Создаем экземпляр класса LoggingAspect
+
+        // Получаем метод doSomething класса MyService
+        Method method = MyService.class.getMethod("doSomething", String.class);
+        // Проверяем, есть ли у метода аннотация @Loggable
+        if (method.isAnnotationPresent(Loggable.class)) {
+            // Логируем вызов метода с аргументом "testParam"
+            aspect.logMethodCall(method, new Object[]{"testParam"});
         }
+        // Вызываем метод doSomething с аргументом "testParam"
+        method.invoke(service, "testParam");
     }
 }
